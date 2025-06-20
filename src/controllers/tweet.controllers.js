c76e2d9a30b57f817f6735f8e0e4c3003165f796
Pritesh Thorat <thoratpritesh1@gmail.com>
2025-06-20 13:04:31 +0530

    TODO Completed: create tweet

---
+++ src/controllers/tweet.controllers.js
@@ -0,0 +1,45 @@
+import mongoose, { isValidObjectId } from "mongoose"
+import { Tweet } from "../models/tweet.models.js"
+import { User } from "../models/user.models.js"
+import ApiError from "../utils/ApiError.js"
+import ApiResponse from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+
+const createTweet = asyncHandler(async (req, res) => {
+    //TODO: create tweet
+    const { content } = req.body
+
+    if(!content)
+        throw new ApiError(404, "Content is required")
+
+    const tweet = await Tweet.create({
+        owner: req.user?._id,
+        content
+    })
+
+    if(!tweet)
+        throw new ApiError(500, "Something went wrong while saving data")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, tweet, "Tweet Created successfully"))
+})
+
+const getUserTweets = asyncHandler(async (req, res) => {
+    // TODO: get user tweets
+})
+
+const updateTweet = asyncHandler(async (req, res) => {
+    //TODO: update tweet
+})
+
+const deleteTweet = asyncHandler(async (req, res) => {
+    //TODO: delete tweet
+})
+
+export {
+    createTweet,
+    getUserTweets,
+    updateTweet,
+    deleteTweet
+}
